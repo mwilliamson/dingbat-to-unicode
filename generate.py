bd99365acdfbac8a6546a3dea972dc6e180690a2
Michael Williamson <mike@zwobble.org>
2021-01-19 21:03:55 +0000

    Extract code for writing JavaScript module

--- generate.py
+++ generate.py
@@ -9,6 +9,17 @@ _root = os.path.dirname(__file__)
 def _main():
     dingbats = _read_dingbats()
 
+    _write_javascript_module(dingbats)
+
+
+def _read_dingbats():
+    with open(os.path.join(_root, "dingbats.csv")) as dingbats_file:
+        dingbat_reader = csv.DictReader(dingbats_file)
+
+        return list(dingbat_reader)
+
+
+def _write_javascript_module(dingbats):
     with open(os.path.join(_root, "js/src/dingbats.ts"), "wt", encoding="utf-8") as typescript_file:
         typescript_file.write("const dingbats = [\n")
 
@@ -19,13 +30,6 @@ def _main():
 
         typescript_file.write("];\n")
         typescript_file.write("export default dingbats;\n");
-
-
-def _read_dingbats():
-    with open(os.path.join(_root, "dingbats.csv")) as dingbats_file:
-        dingbat_reader = csv.DictReader(dingbats_file)
-
-        return list(dingbat_reader)
 
 
 if __name__ == "__main__":
